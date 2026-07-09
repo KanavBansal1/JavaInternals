f1f06b92fbf5b46821b8cffab19c25064d4526cc
Kanav Bansal <kanav.bansal@geekyants.com>
2026-07-10 00:35:40 +0530

    Queue Implementation using linkedList

--- MyQueue.java
+++ MyQueue.java
@@ -1,6 +1,72 @@
-public class MyQueue{
+public class MyQueue {
 
-    private MyLinkedList list;
+    private class Node {
+        Object data;
+        Node next;
 
-    
+        Node(Object data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void enqueue(Object element) {
+        Node newNode = new Node(element);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public Object dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        Object removed = head.data;
+        head = head.next;
+        if (head == null) tail = null; 
+        size--;
+        return removed;
+    }
+
+    public Object peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return head.data;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) sb.append(", ");
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
